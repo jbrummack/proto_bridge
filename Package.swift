dc38beae5f99865db8ffc65d58ae99cfbcc84e4a
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2024-10-04 17:19:06 +0200

    Initial Commit
    iOS Support:
    - setup protoc for Swift
    - SPM Package

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "proto_bridge",
+    products: [
+        .library(name: "proto_bridge", targets: ["proto_bridge"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.27.0")
+    ],
+    targets: [
+        .target(
+            name: "proto_bridge",
+            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")]
+        )
+    ]
+)
