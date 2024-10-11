502be313af651648175cc41c6b928fdb5afdc8f8
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2024-10-11 16:28:46 +0200

    working on codegen templates

--- src/bin.rs
+++ src/bin.rs
@@ -1,44 +1,43 @@
-use std::{
-    borrow::Borrow,
-    collections::HashMap,
-    fmt::Display,
-    hash::Hash,
-    ops::{Deref, DerefMut},
-    rc::Rc,
-};
+use std::{collections::HashMap, hash::Hash};
 
 fn main() {
     let interface = include_str!("messaging.interface");
     //let interface = include_str!("messaging.proto");
     //println!("{interface}");
     let classes = tokenize(interface).expect("tokenize err");
-    let mut stm = StateMachine::new();
-    stm.parse_token_stream(classes);
+    let mut stm = InterfaceBuilder::new();
+    let interface_desc = stm.parse_token_stream(classes);
+    println!("{:#?}", interface_desc);
+
+    for (interface_name, interface_impl) in interface_desc {
+        for (func_name, func_impl) in interface_impl.clone() {
+            println!(
+                "{}",
+                func_impl.codegen_rs_bridge(&interface_name, &func_name)
+            );
+        }
+        println!("trait {interface_name} {{");
+        for (func_name, func_impl) in interface_impl {
+            println!("\t{}", func_impl.codegen_rs_trait(&func_name));
+        }
+        println!("}}\n");
+    }
     //println!("{:?}", classes);
 }
 
-enum BridgeError {
+enum _BridgeError {
     Tokenize(TokenizeError),
     Codegen,
 }
 
-struct Interface {
-    class_definitions: std::collections::HashMap<String, Option<Class>>,
-}
-
-struct Class {
-    name: String,
-    members: std::collections::HashMap<String, Function>,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Function {
     is_async: bool,
     input: Vec<FunctionParam>,
     output: Vec<String>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct FunctionParam {
     param_name: String,
     param_type: String,
@@ -48,9 +47,21 @@ impl std::fmt::Display for FunctionParam {
         write!(f, "{}: {}", self.param_name, self.param_type)
     }
 }
-use convert_case::{Case, Casing};
 impl Function {
-    fn codegen_rs(&self, name: &str) -> () {
+    fn codegen_rs_bridge(self, class_name: &str, name: &str) -> String {
+        let strfied_params_export: Vec<String> = self
+            .input
+            .clone()
+            .into_iter()
+            .map(|prm| format!("{}: MessageFatPointer", prm.param_name))
+            .collect();
+        format!(
+            "pub extern \"C\" fn _internal_{class_name}_{name}({}) -> ({}) {{}}",
+            strfied_params_export.join(", "),
+            self.output.join(", ")
+        )
+    }
+    fn codegen_rs_trait(&self, name: &str) -> String {
         let is_async = if self.is_async { "async " } else { "" };
         let strfied_params: Vec<String> = self
             .input
@@ -58,29 +69,19 @@ impl Function {
             .into_iter()
             .map(|prm| format!("{prm}"))
             .collect();
-        let strfied_params_export: Vec<String> = self
-            .input
-            .clone()
-            .into_iter()
-            .map(|prm| format!("{}: MessageFatPointer", prm.param_name))
-            .collect();
-        println!(
+        format!(
             "{is_async}fn {name}({}) -> ({}) {{}}",
             strfied_params.join(", "),
             self.output.join(", ")
-        );
-        println!(
-            "pub extern \"C\" fn _internal_{name}({}) -> ({}) {{}}",
-            strfied_params_export.join(", "),
-            self.output.join(", ")
-        );
+        )
+        /**/
     }
 }
 
-struct TokenPos {
+/*struct TokenPos {
     line_number: usize,
     position: usize,
-}
+}*/
 
 #[derive(Debug, Clone, PartialEq, Hash, Eq)]
 enum Token {
@@ -169,8 +170,9 @@ impl std::fmt::Display for Token {
     }
 }
 
-#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+#[derive(Debug, PartialEq, Hash, Eq, Clone, Default)]
 enum StateMachine {
+    #[default]
     State0,
     ProtoBuf,
     ParsingClass,
@@ -186,29 +188,45 @@ enum StateMachine {
     ParsingFunctionResultSemicolon,
 }
 
-impl StateMachine {
-    fn new() -> Self {
-        StateMachine::State0
+type InterfaceClass = HashMap<String, Function>;
+type Interface = HashMap<String, InterfaceClass>;
+
+#[derive(Debug, Default)]
+struct InterfaceBuilder {
+    state: StateMachine,
+    classes: Interface,
+    current_i_class: InterfaceClass,
+    current_function_name: String,
+    current_param: FunctionParam,
+    current_function_args: Vec<FunctionParam>,
+    current_class: String,
+    current_function_results: Vec<String>,
+    protobuf_depth: i32,
+    is_async: bool,
+}
+impl InterfaceBuilder {
+    pub fn new() -> Self {
+        InterfaceBuilder::default()
     }
-    fn parse_token_stream(&mut self, ts: Vec<Token>) {
+
+    fn parse_token_stream(&mut self, ts: Vec<Token>) -> Interface {
         type Sm = StateMachine;
         type Tk = Token;
-        //let mut token = Token::Class;
-        let mut state = StateMachine::new();
-        let mut current_class: String = String::new();
-        let mut current_function_name: String = String::new();
-        let mut current_param: FunctionParam = FunctionParam {
-            param_name: String::new(),
-            param_type: String::new(),
-        };
-        let mut current_function_args: Vec<FunctionParam> = Vec::new();
-        let mut current_function_results: Vec<String> = Vec::new();
-        let mut protobuf_depth = 0;
-        let mut is_async = false;
+
         for token in ts {
-            state = match (&state, &token) {
+            self.state = match (&self.state, &token) {
                 (ignore, Tk::Whitespace) => ignore.clone(),
-                (Sm::State0, Tk::Class) => Sm::ParsingClass,
+                (Sm::State0, Tk::Class) => {
+                    if !self.current_i_class.is_empty() {
+                        let move_out = self.current_i_class.clone();
+                        println!("{:?}", move_out);
+                        self.classes.insert(self.current_class.to_owned(), move_out);
+                        println!("reset i class");
+                        self.current_i_class = InterfaceClass::new();
+                        self.current_class = String::new();
+                    }
+                    Sm::ParsingClass
+                }
 
                 (Sm::State0, Tk::Message) => {
                     print!("message");
@@ -217,14 +235,14 @@ impl StateMachine {
                 (Sm::State0, _) => Sm::State0,
 
                 (Sm::ProtoBuf, Tk::CurlyOpen) => {
-                    protobuf_depth += 1;
+                    self.protobuf_depth += 1;
                     print!("{{\n");
                     Sm::ProtoBuf
                 }
                 (Sm::ProtoBuf, Tk::CurlyClose) => {
-                    protobuf_depth += -1;
+                    self.protobuf_depth += -1;
                     print!("}}\n");
-                    if protobuf_depth == 0 {
+                    if self.protobuf_depth == 0 {
                         Sm::State0
                     } else {
                         Sm::ProtoBuf
@@ -235,21 +253,27 @@ impl StateMachine {
                     Sm::ProtoBuf
                 }
                 (Sm::ParsingClass, Tk::Word(class_name)) => {
-                    current_class = class_name.to_owned();
+                    self.current_class = dbg!(class_name.to_owned());
                     Sm::ParsingClassFunctions
                 }
                 (Sm::ParsingClassFunctions, Tk::CurlyOpen) => Sm::ParsingFunctionName,
                 (Sm::ParsingFunctionName, Tk::Word(name)) => {
-                    current_function_name = name.to_owned();
+                    self.current_function_name = dbg!(name.to_owned());
+
                     Sm::ParsingFunctionArgs
                 }
-                (Sm::ParsingFunctionName, Tk::CurlyClose) => Sm::State0,
+                (Sm::ParsingFunctionName, Tk::CurlyClose) => {
+                    let move_out = self.current_i_class.clone();
+                    println!("MOVE OUT I CLASS = {:?}", move_out);
+                    self.classes.insert(self.current_class.to_owned(), move_out);
+                    Sm::State0
+                }
                 (Sm::ParsingFunctionArgs, Tk::ParenOpen) => Sm::ParsingFunctionArgsWord,
 
                 (Sm::ParsingFunctionArgsWord, Tk::ParenClose) => Sm::ParsingAsync,
                 (Sm::ParsingFunctionArgsWord, Tk::Word(arg)) => {
                     //current_function_args.push(arg.to_owned());
-                    current_param.param_name = arg.to_owned();
+                    self.current_param.param_name = arg.to_owned();
                     Sm::ParsingFunctionArgsName
                 }
                 (Sm::ParsingFunctionArgsName, Tk::Colon) => {
@@ -259,14 +283,14 @@ impl StateMachine {
                 }
                 (Sm::ParsingFunctionArgsType, Tk::Word(arg)) => {
                     //current_function_args.push(arg.to_owned());
-                    current_param.param_type = arg.to_owned();
-                    current_function_args.push(current_param.clone());
+                    self.current_param.param_type = arg.to_owned();
+                    self.current_function_args.push(self.current_param.clone());
                     Sm::ParsingFunctionArgsWord
                 }
                 (Sm::ParsingFunctionArgsWord, Tk::Comma) => Sm::ParsingFunctionArgsWord,
 
                 (Sm::ParsingAsync, Tk::Async) => {
-                    is_async = true;
+                    self.is_async = true;
                     Sm::ParsingAsync
                 }
                 (Sm::ParsingAsync, Tk::Arrow) => Sm::ParsingFunctionResult,
@@ -276,26 +300,37 @@ impl StateMachine {
                     Sm::ParsingFunctionResultSemicolon
                 }
                 (Sm::ParsingFunctionResultWord, Tk::Word(res)) => {
-                    current_function_results.push(res.to_owned());
+                    self.current_function_results.push(res.to_owned());
                     Sm::ParsingFunctionResultWord
                 }
                 (Sm::ParsingFunctionResultWord, Tk::Comma) => Sm::ParsingFunctionResultWord,
 
                 (Sm::ParsingFunctionResultSemicolon, Tk::Semicolon) => {
                     let func = Function {
-                        is_async,
-                        input: current_function_args.clone(),
-                        output: current_function_results.clone(),
+                        is_async: self.is_async,
+                        input: self.current_function_args.clone(),
+                        output: self.current_function_results.clone(),
                     };
+                    self.current_i_class
+                        .insert(self.current_function_name.clone(), func.clone());
+                    /*self.add_fn(
+                        self.current_class.clone(),
+                        self.current_function_name.clone(),
+                        func,
+                    );*/
+                    //func.codegen_rs(&self.current_function_name);
 
-                    func.codegen_rs(&current_function_name);
-
-                    is_async = false;
-                    current_function_args = Vec::new();
-                    current_function_results = Vec::new();
+                    self.is_async = false;
+                    self.current_function_args = Vec::new();
+                    self.current_function_results = Vec::new();
                     Sm::ParsingFunctionName
                 }
-                (Sm::ParsingClass, Tk::CurlyClose) => Sm::State0,
+                (Sm::ParsingClass, Tk::CurlyClose) => {
+                    let move_out = self.current_i_class.clone();
+                    println!("MOVE OUT I CLASS = {:?}", move_out);
+                    self.classes.insert(self.current_class.to_owned(), move_out);
+                    Sm::State0
+                }
 
                 //(Sm::ParsingClassFunctions, Tk::CurlyClose) => Sm::State0,
                 _ => {
@@ -303,41 +338,33 @@ impl StateMachine {
                     break;
                 }
             };
-            //println!("{:?}:{:?}", token, state);
+            //println!("==============\n {:?} \n==============", self);
+            //println!("{:?}:{:?}", token, self.state);
         }
+        self.classes.to_owned()
     }
 }
 
-/*fn preprocess(input: &str) -> String {
-    let preprocess_input = input
-        .replace(";", " ; ")
-        .replace("(", " ( ")
-        .replace(")", " ) ")
-        .replace("{", " {} ")
-        .replace("}", " } ")
-        .replace("->", " -> ");
-    return preprocess_input;
-}*/
 #[derive(Debug)]
 enum TokenizeError {}
 
 fn tokenize(input: &str) -> Result<Vec<Token>, TokenizeError> {
-    let mut ct_line = 0;
-    let mut ct_pos = 0;
+    //let mut ct_line = 0;
+    //let mut ct_pos = 0;
     let mut res = Vec::new();
-    let mut pos: Vec<TokenPos> = Vec::new();
+    //let mut pos: Vec<TokenPos> = Vec::new();
     let mut wordbuf = String::new();
-    let mut is_comment = false;
+    //let mut is_comment = false;
     for char in input.chars() {
-        ct_pos += 1;
+        /*ct_pos += 1;
         if char == '\n' {
             ct_line += 1;
             ct_pos = 0;
         }
-        let current_pos = TokenPos {
+        let _current_pos = TokenPos {
             line_number: ct_line,
             position: ct_pos,
-        };
+        };*/
         //println!("{wordbuf}");
         if wordbuf == "class" && !char.is_alphanumeric() {
             res.push(Token::Class);
@@ -369,6 +396,13 @@ fn tokenize(input: &str) -> Result<Vec<Token>, TokenizeError> {
                     wordbuf = String::new();
                 }
                 res.push(Token::EqSign);
+            }
+            '/' => {
+                if !wordbuf.is_empty() {
+                    res.push(Token::Word(wordbuf));
+                    wordbuf = String::new();
+                }
+                res.push(Token::Slash);
             }
             '.' => {
                 if !wordbuf.is_empty() {
